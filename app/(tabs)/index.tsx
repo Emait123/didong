a99ee117f91e2b2b2cf0ded6ec12a808cdf8d190
Tiến Đạt <ttdat197@gmail.com>
2024-11-17 00:01:23 +0700

    flex layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,10 +8,21 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   const darkMode = useColorScheme() === 'dark'
   return (
-    <View style={styles.container}>
-      <Text style={darkMode ? styles.whiteText : styles.darkText}>
-        Text duoc style bang Stylesheet abc
+    <View style={{flex: 1}}>
+      <Text style={[darkMode ? styles.whiteText : styles.darkText, styles.header]}>
+        Flat Cards
       </Text>
+      <View style={styles.container}>
+        <View style={[styles.whiteText, styles.card, styles.card1]}>
+          <Text>Card 1</Text>
+        </View>
+        <View style={[styles.whiteText, styles.card, styles.card2]}>
+          <Text>Card 2</Text>
+        </View>
+        <View style={[styles.whiteText, styles.card, styles.card3]}>
+          <Text>Card 3</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -20,6 +31,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    flexDirection: 'row',
+    padding: 8
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 50,
+    paddingStart: 30,
   },
   whiteText: {
     paddingTop: 50,
@@ -28,5 +47,23 @@ const styles = StyleSheet.create({
   darkText: {
     paddingTop: 50,
     color: '#000000'
-  }
+  },
+  card: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: 4,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card1: {
+    backgroundColor: '#EF5354'
+  },
+  card2: {
+    backgroundColor: '#50DBB4'
+  },
+  card3: {
+    backgroundColor: '#5DA3FA'
+  },
 });
